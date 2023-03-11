8bdd713b21d7bbce85a3cde6008913c511fc2773
soso <127365917+sosolol@users.noreply.github.com>
2023-03-10 20:25:57 -0500

    Update 300K-Nightclub-Vault.lua

--- Recovery/300K-Nightclub-Vault.lua
+++ Recovery/300K-Nightclub-Vault.lua
@@ -1,10 +1,5 @@
-local nightclub = {
-    safe = globals.get_int(262145 + 24045);
-    income = globals.get_int(262145 + 24041)
-}
-
-globals.set_int((262145 + 24045), 300000)
-globals.set_int((262145 + 24041), 300000)
-
-STATS.STAT_SET_INT("CLUB_PAY_TIME_LEFT", 1, true)
-STATS.STAT_SET_INT("CLUB_POPULARITY", 10000, true)
+LP = globals.get_int(1574918)
+if (LP == 0) 
+    then  STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_CLUB_PAY_TIME_LEFT"), -1, true); STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_CLUB_POPULARITY"), 1000, true)
+    else  STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_CLUB_PAY_TIME_LEFT"), -1, true); STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_CLUB_POPULARITY"), 1000, true)
+end
