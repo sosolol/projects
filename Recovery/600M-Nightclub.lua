498388b9470120ca97686951765d9905cffe9a3a
soso <127365917+sosolol@users.noreply.github.com>
2023-03-10 20:26:16 -0500

    Update 600M-Nightclub.lua

--- Recovery/600M-Nightclub.lua
+++ Recovery/600M-Nightclub.lua
@@ -1,7 +1,5 @@
-P1 = MISC.GET_HASH_KEY("MP0_PROP_NIGHTCLUB_VALUE"); P2 = MISC.GET_HASH_KEY("MP1_PROP_NIGHTCLUB_VALUE"); LP = globals.get_int(1574918)
-menu.show_message("Lua", "Mazebank Foreclosure -> Buy another nightclub -> value should be modded on trade in")
-
+LP = globals.get_int(1574918)
 if (LP == 0) 
-    then STATS.STAT_SET_INT(P1, ((1000000000*2) + 4500000), true);
-    else STATS.STAT_SET_INT(P2, ((1000000000*2) + 4500000), true);
+    then STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_PROP_NIGHTCLUB_VALUE"), 19990000, true);
+    else STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_PROP_NIGHTCLUB_VALUE"), 19990000, true);
 end
